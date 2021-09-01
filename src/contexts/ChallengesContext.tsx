008f957ccaaa9991a9bd1ce9547acb2ef2008a9e
Silvio Monnerat <silviomonnerat@gmail.com>
2021-08-31 23:23:48 -0300

    first commit

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,119 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import Cookies from 'js-cookie';
+
+import challenges from '../../challenges.json';
+import { LevelUpModal } from "../components/LevelUpModal";
+
+interface challengeProps {
+  type: string;
+  description: string;
+  amount: number;
+}
+
+interface challengesContextDataProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  experienceToNextLevel: number;
+  activeChallenge: challengeProps;
+  handleLevelUp: () => void;
+  handleStartNewChallenge: () => void;
+  resetChallenge: () => void;
+  completeChallenge: () => void;
+  closeLevelUpModal: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export const ChallengesContext = createContext({} as challengesContextDataProps);
+
+export function ChallengesProvider({ children, ...rest }: ChallengesProviderProps) {
+  const [level, setLevel] = useState(rest.level ?? 1);
+  const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
+  const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
+  const [activeChallenge, setActiveChallenge] = useState(null);
+  const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false);
+
+  const experienceToNextLevel =   Math.pow((level + 1) * 4, 2);
+
+  useEffect(() => {
+    Notification.requestPermission();
+  }, []);
+
+  useEffect(() => {
+    Cookies.set('level', String(level));
+    Cookies.set('currentExperience', String(currentExperience));
+    Cookies.set('challengesCompleted', String(challengesCompleted));
+  }, [level, currentExperience, currentExperience]);
+
+  function handleLevelUp() {
+    setLevel(level + 1);
+    setIsLevelUpModalOpen(true);
+  }
+
+  function closeLevelUpModal() {
+    setIsLevelUpModalOpen(false);
+  }
+
+  function handleStartNewChallenge() {
+    const challengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[challengeIndex];
+
+    setActiveChallenge(challenge);
+
+    new Audio('/notification.mp3').play();
+
+    if (Notification.permission === "granted") {
+      new Notification('Novo desafio 🎉', {
+        body: `Valendo ${challenge.amount}xp`
+      });
+    }
+  }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
+  }
+
+  function completeChallenge() {
+    if (!activeChallenge) return;
+
+    const { amount } = activeChallenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if (finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel;
+      handleLevelUp();
+    }
+
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
+  }
+
+  return (
+    <ChallengesContext.Provider
+      value={{
+        level,
+        currentExperience,
+        challengesCompleted,
+        handleLevelUp,
+        handleStartNewChallenge,
+        activeChallenge,
+        resetChallenge,
+        experienceToNextLevel,
+        completeChallenge,
+        closeLevelUpModal
+      }}
+    >
+      {children}
+
+      { isLevelUpModalOpen && <LevelUpModal /> }
+    </ChallengesContext.Provider>
+  )
+}
---
+++ src/contexts/CountdownContext.tsx
@@ -0,0 +1,68 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { ChallengesContext } from "./ChallengesContext";
+
+interface CountdownContextDataProps {
+  minutes: number;
+  seconds: number;
+  isActive: boolean;
+  hasFinished: boolean;
+  startCountdown: () => void;
+  resetCountdown: () => void;
+}
+
+interface CountdownProviderProps {
+  children: ReactNode;
+}
+
+export const CountdownContext = createContext({} as CountdownContextDataProps);
+
+let countdownTimeout:NodeJS.Timeout;
+
+export function CountdownProvider({ children }: CountdownProviderProps) {
+  const { handleStartNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  function startCountdown() {
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setHasFinished(false);
+    setTime(25 * 60);
+  }
+
+  useEffect(() => {
+    if (isActive &&  time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      handleStartNewChallenge();
+    }
+  }, [isActive, time]);
+
+  return (
+    <CountdownContext.Provider
+      value={{
+        minutes,
+        seconds,
+        isActive,
+        hasFinished,
+        startCountdown,
+        resetCountdown,
+      }}
+    >
+      {children}
+    </CountdownContext.Provider>
+  )
+}
